7722a73b6416bfcdf905ac01ffb2cd36101b4d9d
oelizondo <oscarmarcelo95@gmail.com>
2016-11-15 21:53:18 -0600

    correct url

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+var jsLibrary = $('#js-library')
+var URL = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+
 var generateWord = function() {
-  var jsLibrary = $('#js-library')
-  var URL = "https://www.setgetgo.com/randomword/get.php"
 
   $.ajax(URL)
-   .done(function(word) {
+   .done(function(data) {
+     var word = data.word
      jsLibrary.html(word + '.js')
    })
 }
